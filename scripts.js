438feb3d53fd64f2dbb4bb807187d2890c29ad07
Luana Melo <lsilvaalvesmelo@gmail.com>
2026-04-02 22:31:42 +0100

    Alterando a interface e a responsividade

--- scripts.js
+++ scripts.js
@@ -2,11 +2,11 @@ let list = document.querySelectorAll('.item');
 let next = document.getElementById('next');
 let prev = document.getElementById('prev');
 
-let count = list.length
+let count = list.length;
 let active = 0;
 
 next.onclick = () => {
-    let activeOld = document.querySelector('.active');
+    let activeOld = document.querySelector('.item.active');
     activeOld.classList.remove('active');
 
     active = active >= count - 1 ? 0 : active + 1;
@@ -16,8 +16,7 @@ next.onclick = () => {
 prev.onclick = () => {
     let activeOld = document.querySelector('.item.active');
     activeOld.classList.remove('active');
-    
+
     active = active <= 0 ? count - 1 : active - 1;
     list[active].classList.add('active');
-
 }
